3e71c639e6deee38bb02b9292e7eb8b7a9a8a83e
Erik Behrends <behrends@users.noreply.github.com>
2024-05-02 11:39:07 +0200

    Update to new AI-SDK version

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,26 +1,16 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-// Set the runtime to edge for best performance
-export const runtime = 'edge';
+import { openai } from '@ai-sdk/openai';
+import { StreamingTextResponse, streamText } from 'ai';
 
 export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-4-turbo',
-    stream: true,
+  // Call the language model
+  const result = await streamText({
+    model: openai('gpt-4-turbo'),
     messages,
   });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
   // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(result.toAIStream());
 }
